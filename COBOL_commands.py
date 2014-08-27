67f64292090c1dd11ddb94446f2bccdfd4d5f95f
Stephen Gennard <stephen@gennard.net>
2014-08-27 23:37:03 +0100

    add main/valuetype snippet tweak comment/uncomment command

--- COBOL_commands.py
+++ COBOL_commands.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin,codecs,re
 
 class CobolSequenceNumbersRemoveCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -63,9 +63,54 @@ class CobolRemoveSequenceArea(sublime_plugin.TextCommand):
 
 class CobolFindProcedureDivision(sublime_plugin.TextCommand):
       def run(self, edit):
-        print("CobolFindProcedureDivision")
         proc_div_region = self.view.find('procedure\s*division', 0, sublime.IGNORECASE)
         if not proc_div_region:
             sublime.error_message("COBOL Procedure Division not found")
             return
         self.view.show(proc_div_region)
+
+
+
+class CobolCommentToggleCommand(sublime_plugin.TextCommand):
+    def replaceLine(self, lineContents):
+        pat = re.compile('[0-9a-zA-Z\-][0-9a-zA-Z\-][0-9a-zA-Z\-][0-9a-zA-Z\-][0-9a-zA-Z\-][0-9a-zA-Z\-][\* ]')
+        if pat.match(lineContents):
+                if lineContents[6] == '*':
+                        lineContents = lineContents[0:6] + ' ' + lineContents[7:]
+                else:
+                        lineContents = lineContents[0:6] + '*' + lineContents[7:]
+        else:
+                i = lineContents.find('*>') 
+
+                if i != -1:
+                        lineContents = lineContents[0:i] +  lineContents[2+i:]
+                else:
+                        l = len(lineContents)
+                        if ((l > 6) and (lineContents[0:6] == '      ')):
+                              i=6
+                        else:
+                              i = len(lineContents) - len(lineContents.lstrip())
+                        lineContents = lineContents[0:i] + '*>' + lineContents[i:]
+
+        return lineContents
+
+    def run(self, edit, block):  
+        print('SPG - Skipping -> '+self.view.settings().get('syntax'))
+        if self.view.settings().get('syntax') != 'Packages/COBOL Syntax/COBOL.tmLanguage':
+                self.view.run_command("toggle_comment", {"block": block})
+                return
+
+	
+        for region in self.view.sel():  
+
+            if region.empty():  
+                # Get the selected text  
+                line = self.view.line(region)  
+                lineContents = self.replaceLine(self.view.substr(line))
+                self.view.replace(edit,line, lineContents)
+            else:
+                newContent = ''
+                for line in self.view.split_by_newlines(region):
+                        newContent += self.replaceLine(self.view.substr(line))+"\n"
+                self.view.replace(edit,region, newContent.rstrip('\n'))
+
